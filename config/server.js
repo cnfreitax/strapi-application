319ad1aeb4da0c0154750ba90897d83539f75482
Freitas <victorprogr@gmail.com>
2021-07-10 02:02:18 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a03ae360f552ebbc88352bc1f5feaaad'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6237847-c0a8-4da0-9d56-b8209b5bb865'
+};
